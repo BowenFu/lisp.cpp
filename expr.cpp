075d9b3ad532fadd0da99840fb9acac16162262e
Bowen Fu <missing>
2021-08-15 12:37:18 +0800

    primitiveProcuedure.

--- expr.cpp
+++ expr.cpp
@@ -1,5 +1,10 @@
 #include <cstdlib>
 #include <memory>
+#include <sstream>
+#include <variant>
+#include <iostream>
+#include <numeric>
+#include <vector>
 
 class Expr;
 class Variable;
@@ -17,15 +22,41 @@ class Expr
 {
 public:
     virtual Expr& eval(Env& env) = 0;
+    virtual std::string toString() const = 0;
     virtual ~Expr() = default;
 };
 
-class Literal final : public Expr
+class Literal : public Expr
 {
 public:
     Expr& eval(Env& env) override
     {
         return *this;
+    }
+};
+
+class Number : public Literal
+{
+public:
+    std::variant<int32_t, double> mNumber;
+    template <typename N>
+    Number(N n)
+    : mNumber(n)
+    {}
+    std::string toString() const override
+    {
+        std::ostringstream o;
+        switch (mNumber.index())
+        {
+        case 0:
+            o << std::get<0>(mNumber);
+            break;
+        
+        case 1:
+            o << std::get<1>(mNumber);
+            break;
+        }
+        return o.str();
     }
 };
 
@@ -36,6 +67,7 @@ public:
     {
         return env.lookupVariableValue(*this);
     }
+    std::string toString() const override;
 };
 
 Expr& Env::lookupVariableValue(Variable& variable)
@@ -51,6 +83,7 @@ public:
     {
         return *mContent;
     }
+    std::string toString() const override;
 };
 
 class Assignment final : public Expr
@@ -62,6 +95,7 @@ public:
     {
         return env.setVariableValue(*mVariable, mValue->eval(env));
     }
+    std::string toString() const override;
 };
 
 Expr& Env::setVariableValue(Variable& variable, Expr& value)
@@ -78,6 +112,7 @@ public:
     {
         return env.defineVariable(*mVariable, mValue->eval(env));
     }
+    std::string toString() const override;
 };
 
 Expr& Env::defineVariable(Variable& variable, Expr& value)
@@ -100,33 +135,86 @@ public:
     {
         return isTrue(mPredicate->eval(env)) ? mConsequent->eval(env) : mAlternative->eval(env);
     }
+    std::string toString() const override;
 };
 
 class Lambda final : public Expr
 {
+    std::shared_ptr<Expr> mParameters;
+    std::shared_ptr<Expr> mBody;
 public:
     Expr& eval(Env& env) override;
+    std::string toString() const override;
 };
 
 class Sequence final : public Expr
 {
+    std::shared_ptr<Expr> mActions;
 public:
     Expr& eval(Env& env) override;
+    std::string toString() const override;
 };
 
 class Cond final : public Expr
 {
+    std::shared_ptr<Expr> mClauses;
 public:
     Expr& eval(Env& env) override;
+    std::string toString() const override;
 };
 
 class Application final : public Expr
 {
+    std::shared_ptr<Expr> mOperator;
+    std::shared_ptr<Expr> mOperands;
 public:
     Expr& eval(Env& env) override;
+    std::string toString() const override;
+};
+
+class Procedure
+{
+public:
+    virtual std::shared_ptr<Expr> apply(std::vector<std::shared_ptr<Expr>> const& args) = 0;
+};
+
+class PrimitiveProcedure : public Procedure
+{
+    std::function<std::shared_ptr<Expr>(std::vector<std::shared_ptr<Expr>> const& args)> mImplementation;
+public:
+    template <typename Func>
+    PrimitiveProcedure(Func func)
+    : mImplementation{func}
+    {}
+    std::shared_ptr<Expr> apply(std::vector<std::shared_ptr<Expr>> const& args) override
+    {
+        return mImplementation(args);
+    }
 };
 
 int32_t main()
 {
+    auto mul = [](std::vector<std::shared_ptr<Expr>> const& args)
+    {
+        auto result = std::accumulate(args.begin(), args.end(), 1.0, [](auto p, std::shared_ptr<Expr> const& arg)
+        {
+            auto v = dynamic_cast<Number&>(*arg);
+            switch (v.mNumber.index())
+            {
+            case 0:
+                return p * std::get<0>(v.mNumber);
+            
+            case 1:
+                return p * std::get<1>(v.mNumber);
+            }
+            return 1.0;
+        }
+        );
+        return std::shared_ptr<Expr>(new Number(result)); 
+    };
+    auto a = std::shared_ptr<Expr>(new Number(5));
+    auto b = std::shared_ptr<Expr>(new Number(0.5));
+    auto mulOp = PrimitiveProcedure{mul};
+    std::cout << mulOp.apply({a, b})->toString() << std::endl;
     return 0;
 }
