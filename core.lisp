663e17bff6990ec08303bed19f1798635d9c1eb8
Bowen Fu <missing>
2021-12-16 22:15:47 +0800

    Read lisp file instead.

---
+++ core.lisp
@@ -0,0 +1,14 @@
+(define atom?
+    (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define not
+    (lambda (x)
+    (if x false true)))
+
+(define
+    (- . lst)
+        (cond
+            ((null? (cdr lst)) (* -1 (car lst)))
+            (else (+ (car lst) (* -1 (car (cdr lst)))))
+        ))
